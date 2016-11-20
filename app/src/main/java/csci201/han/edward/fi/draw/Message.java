2a59b7374c09fde9ad313fc29f041532ec02ac4d
Tanuja Mohan <tanujamohan@guest-wireless-upc-1602-10-120-042-039.usc.edu>
2016-11-20 01:03:17 -0800

    client and server

--- app/src/main/java/csci201/han/edward/fi/draw/Message.java
+++ app/src/main/java/csci201/han/edward/fi/draw/Message.java
@@ -7,11 +7,13 @@ public class Message {
     private String name;
     private boolean isDone;
     private int points;
+    private int order;
 
-    public Message(String name, boolean isDone, int points) {
+    public Message(String name, boolean isDone, int points, int order) {
         this.name = name;
         this.isDone = isDone;
         this.points = points;
+        this.order = order;
     }
 
     public String getName() {
@@ -24,6 +26,10 @@ public class Message {
 
     public int getPoints() {
         return points;
+    }
+
+    public int getOrder() {
+        return order;
     }
 
 
